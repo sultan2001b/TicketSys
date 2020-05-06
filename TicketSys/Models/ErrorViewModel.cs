5e033057b0aab1e5fe1c17f5016543a492f5310e
sultan <sultan@flare.sa>
2020-05-06 16:22:08 +0300

    Add project files.

---
+++ TicketSys/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TicketSys.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
